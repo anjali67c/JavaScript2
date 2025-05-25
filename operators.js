c683cbb656dde544785a33b395423b015f10a2e6
Anjali Prajapati <160333990+anjali67c@users.noreply.github.com>
2025-05-25 11:23:07 +0530

    second commit

---
+++ operators.js
@@ -0,0 +1,34 @@
+//Arithmetic operators:
+let a = 9;
+let b= 10;
+
+console.log("a = " , a , " & b = " , b);
+
+console.log("a + b = " , a + b);
+console.log("a - b = " , a - b);
+console.log("a * b = " , a * b);
+console.log("a / b = " , a / b);
+console.log("a % b = " , a % b);
+console.log("a ** b = " , a ** b);
+
+//Unary Operator
+let c = 8;
+let d = 6;
+c++;
+console.log("c = " , c);
+d--;
+console.log("d = " , d);
+
+
+//comparison operators
+let e = 4;
+let f = 6;
+
+console.log("e == f" , e == f);
+console.log("e != f" , e != f);
+console.log("e === f" , e === f);
+
+
+
+
+
