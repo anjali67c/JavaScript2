be6bfef53f5175b7a5f6166f6aa557d0620a6c2a
Anjali Prajapati <160333990+anjali67c@users.noreply.github.com>
2025-05-26 16:12:56 +0530

    new commit

---
+++ function.js
@@ -0,0 +1,19 @@
+//functions
+function newFunction(){
+    console.log('there is an adventure park.');
+}
+newFunction();
+
+function helloWorld(name) {
+    console.log(name);
+}
+helloWorld("Harsh");
+
+//to find the sum of two numbers:
+function sum(x , y){
+    z = x + y;
+    return z;
+}
+
+let val = sum(3 , 4);
+console.log(val);
